0cacbfdca6464edb870706afd037c0de33344828
(ccp) Jake Deery <jdeery@cityplym.ac.uk>
2021-07-23 17:30:28 +0100

    * Switched to Dropbox for 'CDN' as Cloudinary was going to cost a fortune * Added notes in build tools for what is required to run it * Added 75% grid widths * Added inline and block classes * Added 'middle' flexbox class, which centres on the x and y axis * Fixed embed percentages * Fixed paddings and margins of inner to work consistently across platforms * Added spa-credits SASS file * OPACCredits is now empty, and the SPA now handles the site footer * Wrapper overflow is now hidden on both the x and y axis * OPACHeader is now empty, and the SPA now handles the site header (see below re: CustomSearch) * The cart icon is now on the right of the nav with the user info * A home button now exists on the nav, in addition to the Koha button * Tables now stripe correctly * Removed OpacMoreSearches (see below re: CustomSearch) * Added some padding to the site footer * Updated header and footer template files to match what the SPA code expects * Corrected some code and language on the login modal and login pages to reflect changes brought in 20.11 * FAQs is now finished! * Begin Meet the Team page * OpacCustomSearch : replaced the search box on the catalogue to reflect a more Discovery-like service. From an end-user perspective, this makes the catalogue feel more like a library services platform/search engine service * Small misc tweaks

--- dist/sass-build-tools/build.sh
+++ dist/sass-build-tools/build.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
+
+#
+# a script to build the scss files and output them as dist/OPACUserCSS.css
+#
+# required: npm
+# deb:		sudo apt install npm -y
+# brew:		brew install npm
+# winget:	winget install -e --id OpenJS.Nodejs
+#
+# required: sass
+# npm:		sudo npm install -g sass
+#
+
 sass ../../src/css/build.scss:./../OPACUserCSS.css --style compressed
 echo "File built and outputted to dist folder (-1 level)"
